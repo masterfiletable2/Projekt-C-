d5fcc860e5aa2f618c2078c9fb88682ca526fdd1
masterfiletable2 <masterfiletable2@gmail.com>
2019-02-21 21:12:19 +0100

    Figury

---
+++ kolkoKrzyzyk/kolkoKrzyzyk/figury.cs
@@ -0,0 +1,25 @@
+﻿
+namespace kolkoKrzyzyk
+{
+    /// <summary>
+    /// Reprezentacja figur w grze
+    /// </summary>
+    public enum Figury
+    {
+        /// <summary>
+        /// Puste pole.
+        /// </summary>
+        Clear,
+
+        /// <summary>
+        /// Figura krzyzyka "X".
+        /// </summary>
+        X,
+
+        /// <summary>
+        /// Figura koła "O".
+        /// </summary>
+        O,
+    }
+}
+}
